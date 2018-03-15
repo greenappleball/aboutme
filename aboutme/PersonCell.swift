3db3e5ef16c4cd8c7016a1bd332315f76a0b48ab
Dmitri Petrishin <green.apple.ball@gmail.com>
2018-03-15 20:58:35 +0200

    PersonCell: year input enhanced

--- aboutme/PersonCell.swift
+++ aboutme/PersonCell.swift
@@ -15,6 +15,39 @@ class PersonCell: UITableViewCell {
     @IBOutlet weak var avatarImageView: UIImageView!
 
     weak var person: Person?
+
+    fileprivate var datePicker: UIDatePicker?
+    fileprivate var formatter: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd.MM.yyyy"
+        return formatter
+    }
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        datePicker = UIDatePicker(frame: CGRect(x: 0, y: 0, width: frame.width, height: 216))
+        datePicker?.backgroundColor = .white
+        datePicker?.datePickerMode = UIDatePickerMode.date
+
+        let doneToButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(doneHandler(_:)))
+        let spaceToButton = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let cancelToButton = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(cancelHandler(_:)))
+        yearField.makeToolBar(items: [cancelToButton, spaceToButton, doneToButton])
+
+        yearField.inputView = datePicker
+    }
+
+    // MARK:
+    @objc func doneHandler(_ sender: UIBarButtonItem) -> Void {
+        guard let date = datePicker?.date else { return }
+        let text = formatter.string(from: date)
+        yearField.text = text
+        person?.update(with: [#keyPath(Person.year) : text])
+        yearField.resignFirstResponder()
+    }
+
+    @objc func cancelHandler(_ sender: UIDatePicker) -> Void {
+        yearField.resignFirstResponder()
+    }
 }
 
 extension PersonCell: Fillable {
@@ -37,6 +70,11 @@ extension PersonCell: Fillable {
 
 
 extension PersonCell: UITextFieldDelegate {
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if textField == yearField {
+            datePicker?.date = formatter.date(from: person?.year ?? "") ?? Date()
+        }
+    }
 
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         if let text = textField.text {
